2196e96fe969e2ac9df7fac3ec4caa7d49bdeccf
Thong Nguyen <thong.nguyenhuu@asnet.com.vn>
2020-03-23 11:48:27 +0700

    Change version to 0.1.4

--- drf_partial_response/__init__.py
+++ drf_partial_response/__init__.py
@@ -3,6 +3,6 @@ from __future__ import unicode_literals
 """Package for DRF-partial-response"""
 
 __project__ = "drf_partial_response"
-__version__ = "0.1.3"
+__version__ = "0.1.4"
 
 VERSION = __project__ + '-' + __version__
